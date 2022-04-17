4f97f90e4bbea672307dca8840fbb98e563be574
cayto <aidarous1143@gmail.com>
2022-04-17 21:31:58 +0300

    add heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('./api/server.js');
 
 // Create a port here and keep in mind you have to use locally and heroku
 
-const port = process.env.PORT;
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
